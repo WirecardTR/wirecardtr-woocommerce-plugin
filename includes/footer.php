e57593955b24c850e5e83b63712bce9ba1d21bd9
Coşkun Kurtuldu <coskun.kurtuldu@codevist.com>
2018-04-06 12:04:03 +0300

    changed woocommerce plugin folder

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-} 
-?>
-
-<hr/>
-<div align="left" width="600px">
-Destek için <a href="http://developer.wirecard.com.tr"> Wirecard Developer Portal </a>üzerinden veya  integration@3pay.com adresine bir eposta göndererek bize ulaşabilirsiniz.<br/>
